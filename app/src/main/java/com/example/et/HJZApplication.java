d247a2e4ee966365a9fe118131ff8085d663609b
15535958281 <qq195170>
2019-11-04 18:01:42 +0800

    11/04 86,工具

--- app/src/main/java/com/example/et/HJZApplication.java
+++ app/src/main/java/com/example/et/HJZApplication.java
@@ -5,29 +5,27 @@ import android.app.Application;
 import android.content.Context;
 import android.content.res.Configuration;
 
+import com.example.et.Activity.SplashActivity;
+import com.example.et.Ustlis.SimulatorUtils;
 import com.example.et.Ustlis.Utils;
 import com.example.et.Ustlis.internationalization.LocalManageUtil;
-
-
 
 
 public class HJZApplication extends Application {
 
 
-
     @Override
     public void onCreate() {
         super.onCreate();
+        if (!SimulatorUtils.isSimulator(this)) {
+            Utils.init(this);//工具包相关初始化
+        }
 
-        Utils.init(this);//工具包相关初始化
-        //初始国际语言
-        LocalManageUtil.setApplicationLanguage(this);
 
     }
 
 
-
-       @Override
+    @Override
     protected void attachBaseContext(Context base) {
         //保存系统选择语言
         LocalManageUtil.saveSystemCurrentLanguage(base);
@@ -40,7 +38,6 @@ public class HJZApplication extends Application {
         //保存系统选择语言
         LocalManageUtil.onConfigurationChanged(getApplicationContext());
     }
-
 
 
 }
